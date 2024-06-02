4f3f19734992d67fd9f2dbaede3490789695ed91
omiku <47567246+omiku@users.noreply.github.com>
2024-06-02 14:18:01 +0800

    add CharacterLevel

--- connector-basic/basic.go
+++ connector-basic/basic.go
@@ -59,6 +59,7 @@ type ConnectorConfig struct {
 	UserUsernameJsonPath    string `json:"user_username_json_path"`
 	UserEmailJsonPath       string `json:"user_email_json_path"`
 	UserAvatarJsonPath      string `json:"user_avatar_json_path"`
+	CharacterLevel          int    `jspn:"user_characterLevel"`
 
 	CheckEmailVerified    bool   `json:"check_email_verified"`
 	EmailVerifiedJsonPath string `json:"email_verified_json_path"`
